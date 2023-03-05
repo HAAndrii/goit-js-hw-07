7b386a99058c74e6d290d68cdc1f70002e7b9eba
Andrii Kharchenko <107865478+HAAndrii@users.noreply.github.com>
2023-03-05 09:43:52 +0200

    task1

---
+++ js/01-gallery.js
@@ -0,0 +1,66 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryEl = document.querySelector('.gallery');
+
+const items = itemsCreate(galleryItems)
+
+function itemsCreate (galleryItems) {
+    const arr = galleryItems.map(({ preview, original, description }) => {
+        //console.log(preview, original, description);
+    return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div >`;
+    })
+
+    return arr.join('');
+}
+
+galleryEl.insertAdjacentHTML('afterbegin', items);
+
+galleryEl.addEventListener('click', onGalleryClick);
+
+let imageInstance = 0;
+
+function onGalleryClick(event) {
+    event.preventDefault();
+    
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const originalImg = event.target.dataset.source;
+
+    
+    imageInstance = basicLightbox.create(`<img src="${originalImg}">`,{
+        onShow: addEventKeyUp, 
+        onClose: removeEventKeyUp
+		});
+
+    imageInstance.show();
+}
+
+function addEventKeyUp() {
+    document.addEventListener('keyup', closeModal);
+}
+
+function removeEventKeyUp() {
+    document.removeEventListener('keyup', closeModal);
+}
+
+function closeModal({code}) {
+    if (code !== 'Escape') {
+        return;
+    }
+
+    imageInstance.close();
+}
+
+// console.log(galleryItems);
